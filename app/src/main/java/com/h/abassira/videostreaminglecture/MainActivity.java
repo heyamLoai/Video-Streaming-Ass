06a3531bdb138a1446e50757cc96013516aa3203
heyamLoai <huyam.loai@gmail.com>
2022-04-26 15:37:19 +0300

    Ass-viedo-Streaming

--- app/src/main/java/com/h/abassira/videostreaminglecture/MainActivity.java
+++ app/src/main/java/com/h/abassira/videostreaminglecture/MainActivity.java
@@ -13,44 +13,50 @@ import com.h.abassira.videostreaminglecture.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    String viedoLink="https://firebasestorage.googleapis.com/v0/b/mcc-course-d99c9.appspot.com/o/pexels-barbara-olsen-7880881.mp4?alt=media&token=f5c5ded6-64d1-4da5-8afd-2c5b728f8ce0";
-// "https://firebasestorage.googleapis.com/v0/b/mcc-course-d99c9.appspot.com/o/production%20ID_3818213.mp4?alt=media&token=145b7f97-a8bf-4122-858f-65e7c7a7353b"
+    String videoLink ="https://firebasestorage.googleapis.com/v0/b/assignment-1-63561.appspot.com/o/WhatsApp%20Video%202022-04-26%20at%203.23.24%20PM.mp4?alt=media&token=0bee1e4d-0d56-4657-9cf3-b4179eecbfd9";
     ActivityMainBinding binding;
     PlayerView pv;
-    SimpleExoPlayer player;
 
-    boolean playwhenReady= true;
-    long currentPosition = 0;
-    int currentWindow= 0;
+    SimpleExoPlayer player ;
+    boolean playWhenReady = true;
+    long curentPostion = 0 ;
+    int curentWindow = 0 ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        pv = binding.pLayerView;
+        pv = binding.playerView;
 
     }
-    private void initPlayer(){
+
+    private void  initPlayer(){
+
         player = new SimpleExoPlayer.Builder(this).build();
         pv.setPlayer(player);
-
-        MediaItem item =  MediaItem.fromUri(viedoLink);
+        MediaItem item = MediaItem.fromUri(videoLink);
         player.setMediaItem(item);
-        player.setPlayWhenReady(playwhenReady);
-        player.seekTo(currentWindow,currentPosition);
+
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(curentWindow , curentPostion);
         player.prepare();
+
 
     }
 
-    private void releasePlayer(){
-        if (player != null){
-            playwhenReady = player.getPlayWhenReady();
-           currentWindow = player.getCurrentWindowIndex();
-           currentPosition = player.getCurrentPosition();
-           player = null;
-        }
 
+
+    private  void  relesePlayer(){
+        if(player != null){
+            playWhenReady = player.getPlayWhenReady();
+            curentWindow = player.getCurrentWindowIndex();
+            curentPostion = player.getCurrentPosition();
+            player = null ;
+
+        }
     }
 
     @Override
@@ -58,24 +64,22 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         initPlayer();
     }
-
     @Override
     protected void onResume() {
         super.onResume();
-        if (player != null){
+        if(player != null){
             initPlayer();
         }
     }
-
     @Override
     protected void onPause() {
         super.onPause();
-        releasePlayer();
+        relesePlayer();
     }
+
     @Override
     protected void onStop() {
         super.onStop();
-        releasePlayer();
+        relesePlayer();
     }
-
 }
